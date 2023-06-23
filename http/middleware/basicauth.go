916d5aab8586c020ec0f00dd4aec15ee1da8ae6e
lianyue <admin@lianyue.org>
2023-06-23 23:21:06 +0800

    v0.4.6

--- http/middleware/basicauth.go
+++ http/middleware/basicauth.go
@@ -5,12 +5,12 @@ import (
 )
 
 type BasicAuth struct {
-	Auths map[string]string
+	Auths    map[string]string
+	Verified func(r *http.Request) bool
 }
 
 func (basicAuth *BasicAuth) Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		var ok bool
 		defer func() {
 			if !ok {
@@ -20,25 +20,32 @@ func (basicAuth *BasicAuth) Handler(next http.Handler) http.Handler {
 			}
 		}()
 
-		// 没有认证
-		var inputUsername string
-		var inputPassword string
-		if inputUsername, inputPassword, ok = r.BasicAuth(); !ok {
-			return
+		// 已认证的
+		if basicAuth.Verified != nil {
+			ok = basicAuth.Verified(r)
 		}
 
-		// 用户名不存在
-		var password string
-		if password, ok = basicAuth.Auths[inputUsername]; !ok {
-			return
-		}
+		if !ok {
+			// 没有认证
+			var inputUsername string
+			var inputPassword string
+			if inputUsername, inputPassword, ok = r.BasicAuth(); !ok {
+				return
+			}
 
-		// 密码不匹配
-		if password != inputPassword {
-			ok = false
-			return
+			// 用户名不存在
+			var password string
+			if password, ok = basicAuth.Auths[inputUsername]; !ok {
+				return
+			}
+
+			// 密码不匹配
+			if password != inputPassword {
+				ok = false
+				return
+			}
+			ok = true
 		}
-		ok = true
 
 		next.ServeHTTP(w, r)
 	})
