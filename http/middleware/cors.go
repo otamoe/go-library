751805f37bb2ea473ecf843c9365771682b65191
lianyue <admin@lianyue.org>
2023-06-29 03:31:30 +0800

    v0.4.7

--- http/middleware/cors.go
+++ http/middleware/cors.go
@@ -8,9 +8,11 @@ import (
 
 type (
 	Cors struct {
-		Methods []string
-		Origins []string
-		MaxAge  int
+		Methods       []string
+		Origins       []string
+		Headers       []string
+		ExposeHeaders []string
+		MaxAge        int
 	}
 )
 
@@ -22,14 +24,23 @@ func (cors *Cors) Handler(next http.Handler) http.Handler {
 	if methods == "" {
 		methods = "*"
 	}
+	headers := strings.Join(cors.Headers, ", ")
+	if headers == "" {
+		headers = "Content-Type, Authorization, User-Agent, Range, If-Match, If-Modified-Since, If-None-Match, If-Range, If-Unmodified-Since, X-Requested-With"
+	}
+
+	exposeHeaders := strings.Join(cors.ExposeHeaders, ", ")
+	if exposeHeaders == "" {
+		exposeHeaders = "Accept-Ranges, Content-Range, Content-Length, Content-Disposition, ETag, Date, X-Chunked-Output, X-Stream-Output"
+	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if origins != "" {
 			w.Header().Set("Access-Control-Allow-Methods", methods)
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, User-Agent, Range, If-Match, If-Modified-Since, If-None-Match, If-Range, If-Unmodified-Since, X-Requested-With")
-			w.Header().Set("Access-Control-Expose-Headers", "Accept-Ranges, Content-Range, Content-Length, Content-Disposition, ETag, Date, X-Chunked-Output, X-Stream-Output")
-			w.Header().Set("Access-Control-Max-Age", maxAge)
+			w.Header().Set("Access-Control-Allow-Headers", headers)
+			w.Header().Set("Access-Control-Expose-Headers", exposeHeaders)
 			w.Header().Set("Access-Control-Allow-Origin", origins)
+			w.Header().Set("Access-Control-Max-Age", maxAge)
 			if r.Method == http.MethodOptions {
 				w.WriteHeader(http.StatusOK)
 				return
